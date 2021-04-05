c593361852e2b04f28d1cef3f4ee8c9a171aaa63
Quentin Lassalle <qlassalle@gmail.com>
2021-04-05 15:48:04 +0200

    Remove useless print and variables

--- placement.py
+++ placement.py
@@ -1,17 +1,14 @@
 import os
 import time
 
-position = [[0, 0], [800, 0], [0, 800], [800, 800]]
 j = 0
 commands = ["u", "i", "j", "k"]
 
 for i in (0, 3):
 	for i in range(0, 3):
-		launchChrome = f"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://eurosport.com/ --args --profile-directory=\"coinlist{j}\" --window-size=839,524 --window-position={position[i][0]},{position[i][1]} &"
-		print(launchChrome)
+		launchChrome = f"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://eurosport.com/ --args --profile-directory=\"coinlist{j}\" &"
 		os.system(launchChrome)
 		time.sleep(5)
-		print("waiting for magnet")
 		cmd = f"osascript -e 'tell application \"System Events\" to keystroke \"{commands[i]}\" using " + "{option down, control down}'"
 		# minimize active window
 		os.system(cmd)
