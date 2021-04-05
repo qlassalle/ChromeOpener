b619783645d46abc9d1243b771b1addce1567818
Quentin Lassalle <qlassalle@gmail.com>
2021-04-05 16:33:25 +0200

    Random sleep to space out requests

--- placement.py
+++ placement.py
@@ -7,9 +7,9 @@ magnet_shortcut = ["u", "i", "j", "k"]
 for j in range(0, 4):
 	for i in range(0, 4):
 		profile_number = random.randint(1,100000)
-		launch_chrome_cmd = f"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://eurosport.com/ --args --profile-directory=Person{profile_number} &"
+		launch_chrome_cmd = f"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://coinlist.co/dashboard --args --profile-directory=Person{profile_number} &"
 		os.system(launch_chrome_cmd)
-		time.sleep(5)
+		time.sleep(random.randint(5,12))
 		cmd = f"osascript -e 'tell application \"System Events\" to keystroke \"{magnet_shortcut[i]}\" using " + "{option down, control down}'"
 		os.system(cmd)
 
